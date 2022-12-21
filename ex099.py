5104b13566e7e140b5d4f98a3cf39d90fbb0a171
PAULO CESAR NORMANDIA <115199995+pcnormandia@users.noreply.github.com>
2022-12-21 14:57:18 -0300

    Update ex099.py

--- ex099.py
+++ ex099.py
@@ -1,18 +1,23 @@
-# Exercício Python 099: Faça um programa que tenha uma função chamada maior(),
+# Exercício Python 099: faça um programa que tenha uma função chamada maior(),
 # que receba vários parâmetros com valores inteiros.
 # Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 
 def maior(*num):
-    pos = 0
     nmaior = 0
-
-    while pos <= len(num):
+    pos = 0
+    print('\nAnalisando os valores passados...')
+    while pos < len(num):
         if pos == 0:
-            nmaior = num[0]
-        elif num[pos] > nmaior:
             nmaior = num[pos]
+        if num[pos] > nmaior:
+            nmaior = num[pos]
+        pos +=1
+
+    print(f'{num}. Foram passados {len(num)} valores e o maior valor é: {nmaior}')
     print('-=-'*10)
 
-
-valores = [2, 5, 10, 0]
-maior(valores)
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
