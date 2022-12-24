d8e4bd2a684efdc5fc98bca149034f5f57bc9d07
PAULO CESAR NORMANDIA <115199995+pcnormandia@users.noreply.github.com>
2022-12-24 11:58:41 -0300

    create ex111

---
+++ ex111/ex111.py
@@ -0,0 +1,11 @@
+# Exercício Python 111: Crie um pacote chamado utilidadesCeV
+# que tenha dois módulos internos chamados moeda e dado.
+# Transfira todas as funções utilizadas nos desafios 107, 108, 109 e 110
+# para o primeiro pacote e mantenha tudo funcionando.
+
+from ex111.utilidadescev import moeda
+
+preco = float(input('Digite um preço: R$ '))
+moeda.resumo(preco, 20, 12)
+
+
