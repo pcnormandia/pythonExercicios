d8c3973de3d4ec12aa6e019dbc357e721293475a
PAULO CESAR NORMANDIA <115199995+pcnormandia@users.noreply.github.com>
2022-12-29 17:57:03 -0300

    update ex115

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -1,12 +1,22 @@
 from ex115.lib.interface import *
+from ex115.lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar novas pessoas', 'Sair do sistema'])
     if resposta == 1:
-        cabecalho('opção 1')
+        # Pessoas cadastradas
+       lerArquivo(arq)
     elif resposta == 2:
-        cabecalho('Opção 2')
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaint('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabecalho('Saindo do sistema... Até logo!')
         break
